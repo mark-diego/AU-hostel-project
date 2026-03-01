4f65907a0ca65575d3fef0a4c679a962396d4a04
mark-diego <masangum@africau.edu>
2026-03-01 18:43:17 +0200

    Create Main entry point for University Room Allocation system

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AUHostel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the University Room Allocation System");
+        }
+    }
+}
